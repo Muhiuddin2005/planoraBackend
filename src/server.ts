b99c3ceee53e6967e2a497e857168b4f7f0ed33e
Planora Developer <developer@planora.app>
2026-06-17 16:00:00 +0600

    feat: initialize application express and socket server setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
+import http from "http";
 import app from "./app";
+import { initSocket } from "./app/utils/socket";
 
 const PORT = process.env.PORT || 5000;
 
 async function bootstrap() {
     try {
-        app.listen(PORT, () => {
+        const server = http.createServer(app);
+        initSocket(server);
+
+        server.listen(PORT, () => {
             console.log(`Planora Server is running on http://localhost:${PORT}`);
         });
     } catch (error) {
